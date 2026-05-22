0efd106b2da1bfc28218c48a928c484bf6ab289f
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-05-21 21:51:38 -0300

    feat: add response DTOs

---
+++ DTOs/Alunos/AlunoResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace APIAcademia.DTOs.Alunos
+{
+    public record AlunoResponseDTO(
+        int AlunoId,
+        string Nome,
+        string Email,
+        string ImagemURL,
+        bool Ativo,
+        DateTime DataNascimento,
+        int PlanoId,
+        string PlanoNome
+    );
+}
---
+++ DTOs/Planos/PlanoResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace APIAcademia.DTOs.Planos
+{
+    public record PlanoResponseDTO(
+        int PlanoId,
+        string PlanoNome,
+        string ImagemURL,
+        string Descricao,
+        decimal Mensalidade,
+        int TotalAlunos
+    );
+}
