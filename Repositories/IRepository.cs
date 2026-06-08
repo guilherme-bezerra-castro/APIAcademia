dcf3986ff085cf36377c0b8880b445f4a2dd45c7
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-06-07 21:32:11 -0300

    feat: add generic respositories

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace APIAcademia.Repositories
+{
+    // Interface genérica do repository para eviar repetição de código
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> ObterTodosAsync();
+        Task<T?> ObterPorIdAsync(int id);
+        Task<T> CriarAsync(T entidade);
+        Task<T?> AtualizarAsync(T entidade);
+        Task RemoverAsync(T entidade);
+        Task SalvarAsync();
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,42 @@
+﻿using APIAcademia.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAcademia.Repositories
+{
+    // Implementação genérica do repository para eviar repetição de código
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context) 
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public Task<T> AtualizarAsync(T entidade)
+        {
+            _context.Entry(entidade).State = EntityState.Modified;
+            return Task.FromResult(entidade);
+        }
+
+        public async Task<T> CriarAsync(T entidade)
+        {
+            await _dbSet.AddAsync(entidade);
+            return entidade;
+        }
+
+        public async Task<T?> ObterPorIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> ObterTodosAsync() => await _dbSet.AsNoTracking().ToListAsync();
+
+        public Task RemoverAsync(T entidade)
+        {
+            _dbSet.Remove(entidade);
+            return Task.CompletedTask;
+        }
+
+        public async Task SalvarAsync() => await _context.SaveChangesAsync();
+    }
+}
