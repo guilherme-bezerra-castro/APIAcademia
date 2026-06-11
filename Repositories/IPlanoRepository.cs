f9a7cb815301dffa4e307258987392ffb7f9675d
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-06-10 23:06:58 -0300

    feat: add repository for Plano

---
+++ Repositories/IPlanoRepository.cs
@@ -0,0 +1,10 @@
+﻿using APIAcademia.Models;
+
+namespace APIAcademia.Repositories
+{
+    public interface IPlanoRepository : IRepository<Plano>
+    {
+        Task<IEnumerable<Plano>> ObterTodosComAlunosAsync();
+        Task<Plano?> ObterPorIdComAlunosAsync(int id);
+    }
+}
---
+++ Repositories/PlanoRepository.cs
@@ -0,0 +1,17 @@
+﻿using APIAcademia.Context;
+using APIAcademia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAcademia.Repositories
+{
+    public class PlanoRepository : IPlanoRepository, Repository<Plano>
+    {
+        public PlanoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Plano?> ObterPorIdComAlunosAsync(int id) => await _context.Planos!.Include(p => p.Alunos).AsNoTracking().FirstOrDefaultAsync(p => p.PlanoId == id);
+
+        public async Task<IEnumerable<Plano>> ObterTodosComAlunosAsync() => await _context.Planos!.Include(p => p.Alunos).AsNoTracking().ToListAsync();
+    }
+}
