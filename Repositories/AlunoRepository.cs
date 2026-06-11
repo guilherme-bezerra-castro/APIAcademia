115f0c1cd1a146087dc2c350b4ff084696c3c88c
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-06-10 23:00:14 -0300

    feat: add repository for Aluno

---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,41 @@
+﻿using APIAcademia.Context;
+using APIAcademia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAcademia.Repositories
+{
+    public class AlunoRepository : Repository<Aluno>, IAlunoRepository
+    {
+        public AlunoRepository(AppDbContext context) : base(context)
+        {               
+        }
+
+        public async Task<IEnumerable<Aluno>> FiltrarAsync(bool? ativo, int? planoId)
+        {
+            var query = _context.Alunos!.Include(a => a.Planos).AsNoTracking().AsQueryable();
+
+            if (ativo.HasValue)
+                query = query.Where(a => a.Ativo == ativo.Value);
+
+            if (planoId.HasValue)
+                query = query.Where(a => a.PlanoId == planoId.Value);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<(IEnumerable<Aluno> Itens, int Total)> ObterPaginadoAsync(int pagina, int itensPorPagina)
+        {
+            var query = _context.Alunos!.Include(a => a.Planos).AsNoTracking();
+
+            var itens = await query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina).ToListAsync();
+
+            var total = await query.CountAsync();
+
+            return (itens, total);
+        }
+
+        public async Task<Aluno?> ObterPorIdComPlanoAsync(int id) => await _context.Alunos!.Include(a => a.Planos).AsNoTracking().FirstOrDefaultAsync(a => a.AlunoId == id);
+
+        public async Task<IEnumerable<Aluno>> ObterTodosComPlanoAsync() => await _context.Alunos!.Include(a => a.Planos).AsNoTracking().ToListAsync();
+    }
+}
---
+++ Repositories/IAlunoRepository.cs
@@ -0,0 +1,12 @@
+﻿using APIAcademia.Models;
+
+namespace APIAcademia.Repositories
+{
+    public interface IAlunoRepository : IRepository<Aluno>
+    {
+        Task<IEnumerable<Aluno>> ObterTodosComPlanoAsync();
+        Task<Aluno?> ObterPorIdComPlanoAsync(int id);
+        Task<IEnumerable<Aluno>> FiltrarAsync(bool? ativo, int? planoId);
+        Task<(IEnumerable<Aluno> Itens, int Total)> ObterPaginadoAsync(int pagina, int itensPorPagina);
+    }
+}
