3d288312511c1d2d8cb97bcc2a48e49978a5b0dd
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-06-15 16:45:29 -0300

    feat: add generic DTO

---
+++ DTOs/PaginacaoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace APIAcademia.DTOs
+{
+    public class ResultadoPaginado<T>
+    {
+        public int TotalItens { get; set; }
+        public int TotalPaginas { get; set; }
+        public int PaginaAtual { get; set; }
+        public int ItensPorPagina { get; set; }
+        public IEnumerable<T> Dados { get; set; } = [];
+    }
+}
